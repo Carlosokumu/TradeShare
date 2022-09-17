ead604f2f7a463b6f59f6c17755fde1b14fc35a4
Carlos <carlosokumu254@gmail.com>
2022-09-17 03:49:24 -0700

    More helper functions as extensions

--- app/src/main/java/com/example/smarttrader/Extensions.kt
+++ app/src/main/java/com/example/smarttrader/Extensions.kt
@@ -2,11 +2,13 @@ package com.example.smarttrader
 
 import android.app.Activity
 import android.content.Context
+import android.util.Log
 import android.view.Gravity
 import android.view.View
 import android.widget.Toast
 import com.google.android.material.snackbar.Snackbar
 import com.thecode.aestheticdialogs.*
+import java.util.*
 
 
 fun Context.showErrorToast(message: String){
@@ -50,4 +52,50 @@ fun Context.showToast(message: String) = Toast.makeText(this,message,Toast.LENGT
 
 fun Context.showSnackBar(view: View){
     Snackbar.make(view,"Check your internet Connection",Snackbar.LENGTH_SHORT).show()
+}
+
+fun Context.getDownSideMt4(entryPrice: Double?, stopLoss: Double?): String {
+    return if (entryPrice == null || stopLoss == null) {
+        "-"
+    } else {
+        val difference = entryPrice - stopLoss
+        val percentageDiff = ((difference / entryPrice) * 100)
+        val number3digits: Double = String.format("%.3f", percentageDiff).toDouble()
+        number3digits.toString()
+    }
+}
+
+fun Context. getUpSideMt4(entryPrice: Double?, takeProfit: Double?): String {
+    return if (entryPrice == null || takeProfit == null) {
+        "-"
+    } else {
+        val difference = takeProfit - entryPrice
+        val percentageDiff = ((difference / entryPrice) * 100)
+        val number3digits: Double = String.format("%.3f", percentageDiff).toDouble()
+        number3digits.toString()
+    }
+}
+fun Context.calculateDifference(startDate: Date, endDate: Date): String {
+    var different = endDate.time.minus(startDate.time)
+    val secondsInMilli: Long = 1000
+    val minutesInMilli = secondsInMilli * 60
+    val hoursInMilli = minutesInMilli * 60
+    val daysInMilli = hoursInMilli * 24
+
+    val elapsedDays: Long = different / daysInMilli
+    different = different % daysInMilli
+
+    val elapsedHours: Long = different / hoursInMilli
+    different = different % hoursInMilli
+
+    val elapsedMinutes: Long = different / minutesInMilli
+    different = different % minutesInMilli
+
+    val elapsedSeconds: Long = different / secondsInMilli
+    Log.d(
+        "DIFFERENCE",
+        elapsedDays.toString() + "days" + elapsedHours + "hours" + elapsedMinutes.toString()
+    )
+    return elapsedDays.toString() + "d" + elapsedHours + "hr" + elapsedMinutes.toString() + "min"
+
 }
