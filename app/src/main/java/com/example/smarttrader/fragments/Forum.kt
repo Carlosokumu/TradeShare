272b5270908df0b7a2492c9fe01f8bae50d2755c
Carlos <carlosokumu254@gmail.com>
2022-10-10 00:44:09 -0700

    Toasts for the events

--- app/src/main/java/com/example/smarttrader/fragments/Forum.kt
+++ app/src/main/java/com/example/smarttrader/fragments/Forum.kt
@@ -5,17 +5,53 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.lifecycle.lifecycleScope
 import com.example.smarttrader.R
+import com.example.smarttrader.models.WebSocketState
+import com.example.smarttrader.viewmodels.ForumViewModel
+import com.example.smarttrader.viewmodels.PositionsViewModel
+import kotlinx.coroutines.launch
+import org.koin.android.viewmodel.ext.android.viewModel
 
 
 class Forum : Fragment() {
+
+    private val forumViewModel: ForumViewModel by viewModel()
 
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        forumViewModel.observeConnection()
         // Inflate the layout for this fragment
+        lifecycleScope.launch {
+            forumViewModel.webState.collect { socketState ->
+                when (socketState) {
+                   is WebSocketState.MessageReceived -> {
+
+                   }
+                    is WebSocketState.ConnectionInitializing -> {
+
+                        Toast.makeText(requireContext(),"initializing...",Toast.LENGTH_LONG).show()
+                    }
+                    is WebSocketState.ConnectionFailed -> {
+                        Toast.makeText(requireContext(),"failed...",Toast.LENGTH_LONG).show()
+                    }
+                    is WebSocketState.ConnectionClosing -> {
+                        Toast.makeText(requireContext(),"connection closing...",Toast.LENGTH_LONG).show()
+                    }
+                    is WebSocketState.ConnectionOpened -> {
+                        Toast.makeText(requireContext(),"connection opened",Toast.LENGTH_LONG).show()
+                    }
+                    is WebSocketState.ConnectionClosed -> {
+                        Toast.makeText(requireContext(),"connection closing...",Toast.LENGTH_LONG).show()
+                    }
+                }
+
+            }
+        }
         return inflater.inflate(R.layout.fragment_forum, container, false)
     }
 
