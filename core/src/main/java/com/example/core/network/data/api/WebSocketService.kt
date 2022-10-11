d669c1d975d9dd8af8b605a21f18ff1078c36767
Carlos <carlosokumu254@gmail.com>
2022-10-11 03:55:15 -0700

    SCARLET SEND METHOD

--- core/src/main/java/com/example/core/network/data/api/WebSocketService.kt
+++ core/src/main/java/com/example/core/network/data/api/WebSocketService.kt
@@ -1,7 +1,9 @@
 package com.example.core.network.data.api
 
+import com.example.core.network.data.models.ChatDetails
 import com.tinder.scarlet.WebSocket
 import com.tinder.scarlet.ws.Receive
+import com.tinder.scarlet.ws.Send
 import io.reactivex.Flowable
 
 interface WebSocketService {
@@ -9,4 +11,8 @@ interface WebSocketService {
 
     @Receive
     fun observeConnection(): Flowable<WebSocket.Event>
+
+
+    @Send
+    fun sendMessage(chatDetails: ChatDetails)
 }
