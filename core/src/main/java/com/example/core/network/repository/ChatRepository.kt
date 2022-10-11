d776ab4f0cc1d7788e32c36e418da38de52746f8
Carlos <carlosokumu254@gmail.com>
2022-10-11 03:51:10 -0700

    Added Send method to server

--- core/src/main/java/com/example/core/network/repository/ChatRepository.kt
+++ core/src/main/java/com/example/core/network/repository/ChatRepository.kt
@@ -1,5 +1,6 @@
 package com.example.core.network.repository
 
+import com.example.core.network.data.models.ChatDetails
 import com.tinder.scarlet.WebSocket
 import io.reactivex.Flowable
 
@@ -7,4 +8,7 @@ interface ChatRepository {
 
 
     fun observeConnection(): Flowable<WebSocket.Event>
+
+
+    fun sendMessage(chatDetails: ChatDetails)
 }
--- core/src/main/java/com/example/core/network/repository/ChatRepositoryImp.kt
+++ core/src/main/java/com/example/core/network/repository/ChatRepositoryImp.kt
@@ -2,6 +2,7 @@ package com.example.core.network.repository
 
 import com.example.core.di.webSocketService
 import com.example.core.network.data.api.WebSocketService
+import com.example.core.network.data.models.ChatDetails
 import com.tinder.scarlet.WebSocket
 import io.reactivex.Flowable
 
@@ -11,6 +12,10 @@ class ChatRepositoryImp(private val webSocketService: WebSocketService) : ChatRe
 
     override fun observeConnection(): Flowable<WebSocket.Event> {
        return webSocketService.observeConnection()
+    }
+
+    override fun sendMessage(chatDetails: ChatDetails) {
+        webSocketService.sendMessage(chatDetails = chatDetails)
     }
 
 
