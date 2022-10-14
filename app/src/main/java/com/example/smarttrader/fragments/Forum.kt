4030d4dc890499932385397a9cd0d013c79e652e
Carlos <carlosokumu254@gmail.com>
2022-10-14 03:34:49 -0700

    Code refactoring and build up

--- app/src/main/java/com/example/smarttrader/fragments/Forum.kt
+++ app/src/main/java/com/example/smarttrader/fragments/Forum.kt
@@ -1,12 +1,14 @@
 package com.example.smarttrader.fragments
 
 import android.os.Bundle
+import android.text.Html
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.EditText
 import android.widget.ImageButton
 import android.widget.Toast
+import androidx.core.util.ObjectsCompat
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.lifecycleScope
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -18,11 +20,13 @@ import com.example.smarttrader.models.MessageData
 import com.example.smarttrader.models.MessageType
 import com.example.smarttrader.models.WebSocketState
 import com.example.smarttrader.viewmodels.ForumViewModel
+import com.github.topbottomsnackbar.TBSnackbar
 import com.google.gson.Gson
 import com.tinder.scarlet.Message
 import kotlinx.coroutines.launch
 import org.jetbrains.anko.support.v4.runOnUiThread
 import org.koin.android.viewmodel.ext.android.viewModel
+import java.util.*
 
 
 class Forum : Fragment() {
@@ -54,8 +58,6 @@ class Forum : Fragment() {
         recyclerChats.layoutManager = LinearLayoutManager(requireContext())
         recyclerChats.adapter = chatsAdapter
         btnSend.setOnClickListener {
-//            val data = ChatDetails("carlos", "welcome")
-//            forumViewModel.sendMessage(data)
             sendMessage()
         }
         forumViewModel.observeConnection()
@@ -67,16 +69,38 @@ class Forum : Fragment() {
                     is WebSocketState.MessageReceived -> {
                         val data =
                             Gson().fromJson(socketState.message.toValue(), ChatDetails::class.java)
-                        val messageData = MessageData(chatDetails = data, viewType = MessageType.CHAT_PEER.index)
-                        addItemToRecyclerView(message = messageData)
-                        Toast.makeText(requireContext(), data.username, Toast.LENGTH_LONG).show()
+                        //Update to the current time
+                        data.time = getCurrentTime()
+                        /*
+                              When the message comes back:this means am the sender of the message,so need to  display on recyclerview(haha)
+                         */
+                        if (ObjectsCompat.equals(data.text, editChat.text.toString())) {
+                            editChat.text.clear()
+                        } else {
+                            val messageData =
+                                MessageData(
+                                    chatDetails = data,
+                                    viewType = MessageType.CHAT_PEER.index
+                                )
+                            addItemToRecyclerView(message = messageData)
+                            Toast.makeText(requireContext(), data.username, Toast.LENGTH_LONG)
+                                .show()
+                        }
+
                     }
                     is WebSocketState.ConnectionInitializing -> {
+                        val websocketMessage = Html.fromHtml(getString(R.string.connection_lost))
+                        TBSnackbar.make(
+                            requireActivity().window.decorView,
+                            websocketMessage,
+                            TBSnackbar.LENGTH_SHORT,
+                            TBSnackbar.STYLE_SHOW_TOP_FITSYSTEMWINDOW
+                        ).show();
 
-                        Toast.makeText(requireContext(), "initializing...", Toast.LENGTH_LONG)
-                            .show()
+
                     }
                     is WebSocketState.ConnectionFailed -> {
+
                         Toast.makeText(requireContext(), "failed...", Toast.LENGTH_LONG).show()
                     }
                     is WebSocketState.ConnectionClosing -> {
@@ -84,6 +108,13 @@ class Forum : Fragment() {
                             .show()
                     }
                     is WebSocketState.ConnectionOpened -> {
+                        val websocketMessage = Html.fromHtml(getString(R.string.connected))
+                        TBSnackbar.make(
+                            requireActivity().window.decorView,
+                            websocketMessage,
+                            TBSnackbar.LENGTH_SHORT,
+                            TBSnackbar.STYLE_SHOW_TOP_FITSYSTEMWINDOW
+                        ).show();
                         Toast.makeText(requireContext(), "connection opened", Toast.LENGTH_LONG)
                             .show()
                     }
@@ -114,19 +145,26 @@ class Forum : Fragment() {
         runOnUiThread {
             chatList.add(message)
             chatsAdapter.notifyItemInserted(chatList.size)
-            editChat.setText("")
+            //editChat.setText("")
             recyclerChats.scrollToPosition(chatList.size - 1) //move focus on last message
         }
     }
 
 
     private fun sendMessage() {
+
         val messageInput = editChat.text.toString()
-        val chatDetails = ChatDetails("carlos", messageInput)
+        val chatDetails = ChatDetails("carlos", messageInput, time = getCurrentTime())
         forumViewModel.sendMessage(chatDetails)
         val messageData =
             MessageData(chatDetails, MessageType.CHAT_MINE.index)
         addItemToRecyclerView(messageData)
+
     }
+
+    private fun getCurrentTime() = java.text.DateFormat.getDateTimeInstance().format(Date())
+
+
+    fun getSentTime() = java.text.DateFormat.getDateTimeInstance().format(Date()).drop(12)
 
 }
