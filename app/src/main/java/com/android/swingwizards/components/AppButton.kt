81b85d8cf3fbb55bf96cc4447ff360c51701d4cc
Carlos <carlosokumu254@gmail.com>
2023-11-28 08:25:21 +0300

    feat :- created an app button

---
+++ app/src/main/java/com/android/swingwizards/components/AppButton.kt
@@ -0,0 +1,61 @@
+package com.android.swingwizards.components
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Divider
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import com.android.swingwizards.theme.AppTheme
+
+@Composable
+fun AppButton(
+    modifier: Modifier = Modifier,
+    text: String,
+    onButtonClick: () -> Unit,
+) {
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = modifier
+    ) {
+
+        Divider(color = AppTheme.colors.onSurface)
+        Box(
+            modifier = modifier
+                .padding(
+                    start = 16.dp,
+                    end = 16.dp,
+                    top = 16.dp,
+                    bottom = 30.dp
+                )
+        ) {
+
+            Button(
+                onClick = { onButtonClick() },
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(50.dp),
+                colors = ButtonDefaults.buttonColors(
+                    containerColor = AppTheme.colors.secondaryVariant,
+                    disabledContainerColor = AppTheme.colors.secondaryVariant
+
+                ),
+            ) {
+
+                Text(
+                    text = text,
+                    style = MaterialTheme.typography.caption,
+                    color = AppTheme.colors.textPrimary
+                )
+            }
+        }
+    }
+}
