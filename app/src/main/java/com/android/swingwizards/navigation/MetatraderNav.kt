ad895be09275af21ff38125c5a02f22ffe5b3043
Carlos <carlosokumu254@gmail.com>
2023-12-08 14:44:30 +0300

    change navigation within the app

---
+++ app/src/main/java/com/android/swingwizards/navigation/MetatraderNav.kt
@@ -0,0 +1,66 @@
+package com.android.swingwizards.navigation
+
+import android.content.Context
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import com.android.swingwizards.components.MetaTraderConnectionScreen
+import com.android.swingwizards.components.PasswordReset
+import com.android.swingwizards.components.SignInScreen
+import com.android.swingwizards.components.SignUpComponent
+import com.android.swingwizards.components.SignUpScreen
+import com.android.swingwizards.data.local.entity.TradingPlatformEntity
+import com.android.swingwizards.viewmodels.SignInViewModel
+import com.android.swingwizards.viewmodels.SignUpProcessViewModel
+import com.android.swingwizards.viewmodels.SignUpViewModel
+
+@Composable
+fun MetaTraderNav(
+    navController: NavHostController,
+    startDestination: String = "signup",
+    signUpProcessViewModel: SignUpProcessViewModel,
+    signInViewModel: SignInViewModel,
+    signUpViewModel: SignUpViewModel,
+    context: Context,
+    modifier: Modifier,
+    onTradingPlatformClick: (TradingPlatformEntity) -> Unit
+) {
+    val tradingPlatforms by signUpProcessViewModel.tradingPlatforms.collectAsState()
+    val selectedPlatform by signUpProcessViewModel.selectedPlatform.collectAsState()
+
+    NavHost(
+        navController = navController,
+        startDestination = startDestination,
+        modifier = modifier
+    ) {
+
+
+        composable(route = "signup") {
+            SignUpScreen(navController,signUpViewModel)
+        }
+        composable(route = "signin") {
+            SignInScreen(navController,signInViewModel)
+        }
+        composable(route = "reset") {
+            PasswordReset(navController)
+        }
+
+
+        composable(route = "signupcomponent") {
+            SignUpComponent(
+                tradingPlatformEntity = selectedPlatform,
+                tradingPlatForms = tradingPlatforms,
+                onTradingPlatformClick = { onTradingPlatformClick(it) },
+                navController = navController,
+                context = context
+            )
+        }
+        composable(route = "mt") {
+            MetaTraderConnectionScreen()
+        }
+    }
+}
