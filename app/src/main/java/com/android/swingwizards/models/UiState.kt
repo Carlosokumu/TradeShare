6b3eb5b47ef57a7e27a2b9440e6e23b95c02e3d4
Carlos <carlosokumu254@gmail.com>
2024-01-03 23:43:12 +0300

    make Success more generic

--- app/src/main/java/com/android/swingwizards/models/UiState.kt
+++ app/src/main/java/com/android/swingwizards/models/UiState.kt
@@ -3,7 +3,7 @@ package com.android.swingwizards.models
 import com.example.core.network.data.models.TradeShareUser
 
 sealed class UiState {
-    data class Success(val tradeShareUser: TradeShareUser) : UiState()
+    data class Success<T>(val response: T) : UiState()
     data class Error(val message: String) : UiState()
     data class ServerError(val code: Int?, val message: String) : UiState()
     object Loading : UiState()
