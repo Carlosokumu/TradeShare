5047b1035279b152227d690f469b1f3d5511832f
Carlos <carlosokumu254@gmail.com>
2024-01-04 09:54:15 +0300

    add class to hold different states of input fields

---
+++ app/src/main/java/com/android/swingwizards/models/SignUpStates.kt
@@ -0,0 +1,7 @@
+package com.android.swingwizards.models
+
+
+
+
+
+data class InputFieldsStates(var username: String = "", var password: String  ="", var email: String = "",var metaTraderLogin: String = "",var metaTraderPassword: String = "")
