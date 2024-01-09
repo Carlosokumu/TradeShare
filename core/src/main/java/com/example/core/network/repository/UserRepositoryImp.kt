22e031163307c1cd1245c7e7dd3db3f9764d8217
Carlos <carlosokumu254@gmail.com>
2024-01-09 18:29:20 +0300

    implement api call to fetch a specific user

--- core/src/main/java/com/example/core/network/repository/UserRepositoryImp.kt
+++ core/src/main/java/com/example/core/network/repository/UserRepositoryImp.kt
@@ -1,6 +1,6 @@
 package com.example.core.network.repository
 
-import com.example.core.network.data.api.TradeApi
+import com.example.core.network.data.api.TradeShareApi
 import com.example.core.network.data.api.ApiCallResult
 import com.example.core.network.data.api.MtApi
 import com.example.core.network.data.api.safeApiCall
@@ -8,7 +8,7 @@ import com.example.core.network.data.models.*
 import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.Dispatchers
 
-class UserRepositoryImp(private val tradeApi: TradeApi,
+class UserRepositoryImp(private val tradeShareApi: TradeShareApi,
                         private val mtApi: MtApi,
                         private val ioDispatcher: CoroutineDispatcher = Dispatchers.IO): UserRepository {
 
@@ -22,20 +22,24 @@ class UserRepositoryImp(private val tradeApi: TradeApi,
         userName: String,
         password: String
     ): ApiCallResult<RegisteredUser>  = safeApiCall(ioDispatcher) {
-        return@safeApiCall tradeApi.registerUser(firstname = firstname, lastname = lastname, username = userName, password = password, email = email)
+        return@safeApiCall tradeShareApi.registerUser(firstname = firstname, lastname = lastname, username = userName, password = password, email = email)
     }
 
 
 
     override suspend fun registerTradeShareUser(email: String, username: String, password: String): ApiCallResult<SignUpResponse> = safeApiCall(ioDispatcher){
-        return@safeApiCall tradeApi.registerTradeShareUser(username = username, password = password, email = email)
+        return@safeApiCall tradeShareApi.registerTradeShareUser(username = username, password = password, email = email)
     }
 
     override suspend fun updatePhoneNumber(
         userName: String,
         phoneNumber: String
     ): ApiCallResult<PhoneUpdateResponse> =  safeApiCall(ioDispatcher) {
-        return@safeApiCall tradeApi.updatePhoneNumber(username = userName, phoneNumber = phoneNumber)
+        return@safeApiCall tradeShareApi.updatePhoneNumber(username = userName, phoneNumber = phoneNumber)
+    }
+
+    override suspend fun getTradeShareUser(userName: String): ApiCallResult<TradeShareUserResponse> = safeApiCall(ioDispatcher) {
+        return@safeApiCall tradeShareApi.getTradeShareUser(userName)
     }
 
     override suspend fun getTrades(accountId: String,offset: Int,range: Int): ApiCallResult<Trades> = safeApiCall(ioDispatcher) {
@@ -43,14 +47,14 @@ class UserRepositoryImp(private val tradeApi: TradeApi,
     }
 
     override suspend fun loginUser(userName: String, password: String): ApiCallResult<CurrentUserInfo> = safeApiCall(ioDispatcher) {
-        return@safeApiCall  tradeApi.loginUser(userName,password)
+        return@safeApiCall  tradeShareApi.loginUser(userName,password)
     }
 
     override suspend fun loginTradeShareUser(
         userName: String,
         password: String
     ): ApiCallResult<LoginResponse> = safeApiCall(ioDispatcher) {
-        return@safeApiCall  tradeApi.loginTradeShareUser(userName,password)
+        return@safeApiCall  tradeShareApi.loginTradeShareUser(userName,password)
     }
 
     override suspend fun connectMetaTraderAccount(
@@ -64,15 +68,15 @@ class UserRepositoryImp(private val tradeApi: TradeApi,
     }
 
     override suspend fun sendCode(email: String): ApiCallResult<OtpCode> = safeApiCall(ioDispatcher){
-        return@safeApiCall  tradeApi.sendCode(email)
+        return@safeApiCall  tradeShareApi.sendCode(email)
     }
 
     override suspend fun sendConfirmation(email: String, userName: String): ApiCallResult<Unit> = safeApiCall(ioDispatcher){
-        return@safeApiCall  tradeApi.sendConfirmation(email,userName)
+        return@safeApiCall  tradeShareApi.sendConfirmation(email,userName)
     }
 
     override suspend fun getUserInfo(userName: String): ApiCallResult<CurrentUserInfo> = safeApiCall(ioDispatcher){
-        return@safeApiCall  tradeApi.getUserInfo(userName)
+        return@safeApiCall  tradeShareApi.getUserInfo(userName)
     }
 
 
