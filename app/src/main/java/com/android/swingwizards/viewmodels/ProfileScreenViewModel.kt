ce5d8b49ad41a8f2547a894167d871516af55ee0
Carlos <carlosokumu254@gmail.com>
2024-01-09 17:36:32 +0300

    core :- Profile Screen viewModel

---
+++ app/src/main/java/com/android/swingwizards/viewmodels/ProfileScreenViewModel.kt
@@ -0,0 +1,24 @@
+package com.android.swingwizards.viewmodels
+
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.android.swingwizards.data.repository.UserRepo
+import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.launch
+
+class ProfileScreenViewModel(private val userRepo: UserRepo): ViewModel() {
+
+    private val _userUserName = MutableLiveData<String>()
+    val username = _userUserName
+
+
+
+    fun getUsername() {
+        viewModelScope.launch {
+            userRepo.getUsername.collectLatest { isSignedUp ->
+                _userUserName.value = isSignedUp
+            }
+        }
+    }
+}
