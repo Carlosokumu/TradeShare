331f5ce6846cd331493f51ef36a098ce50a8a3e6
Carlos <carlosokumu254@gmail.com>
2024-01-30 12:07:11 +0300

    improve signin screen

--- app/src/main/java/com/android/swingwizards/components/SignInScreen.kt
+++ app/src/main/java/com/android/swingwizards/components/SignInScreen.kt
@@ -1,5 +1,8 @@
 package com.android.swingwizards.components
 
+import android.content.Context
+import android.util.Log
+import android.widget.Toast
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
@@ -14,26 +17,99 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.drawBehind
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
-import androidx.navigation.compose.rememberNavController
 import com.android.swingwizards.R
-import com.android.swingwizards.common.EmailSection
+import com.android.swingwizards.activities.Dashboard
 import com.android.swingwizards.common.PasswordSection
 import com.android.swingwizards.common.UsernameSection
+import com.android.swingwizards.models.UiState
 import com.android.swingwizards.theme.AppTheme
+import com.android.swingwizards.utils.AppUtils
 import com.android.swingwizards.viewmodels.SignInViewModel
+import com.example.core.network.data.models.LoginResponse
 
 
 @Composable
-fun SignInScreen(navController: NavController, signInViewModel: SignInViewModel) {
+fun SignInScreen(navController: NavController, signInViewModel: SignInViewModel,context: Context) {
     val username = signInViewModel.signInFields.collectAsState().value.username
     val password = signInViewModel.signInFields.collectAsState().value.password
+
+
+
+    val uiState: UiState by signInViewModel.uiState.collectAsState(initial = UiState.Relaxed)
+    var isLoading by remember { mutableStateOf(false) }
+
+
+    when (uiState) {
+        is UiState.Error -> {
+            isLoading = false
+            Toast.makeText(
+                LocalContext.current,
+                "Something Went wrong.Please check your internet connection and try again",
+                Toast.LENGTH_SHORT
+            ).show()
+        }
+
+        UiState.Loading -> {
+            isLoading = true
+        }
+
+        is UiState.Success<*> -> {
+            isLoading = false
+            //Move to Dashboard
+            AppUtils.launchActivity(context = context,Dashboard::class.java)
+            val response = (uiState as UiState.Success<LoginResponse>).response
+            signInViewModel.saveUserUsername(response.user.username)
+            Log.d("ServerResponse:", response.user.username)
+            signInViewModel.saveUserLoggedIn()
+        }
+
+        is UiState.ServerError -> {
+            isLoading = false
+            val serverMessage = (uiState as UiState.ServerError)
+            when(serverMessage.code){
+                 404 -> {
+                    Toast.makeText(
+                        LocalContext.current,
+                        "The given username was not found.",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
+                401 -> {
+                    Toast.makeText(
+                        LocalContext.current,
+                        "The password is incorrect.",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }else -> {
+                Toast.makeText(
+                    LocalContext.current,
+                    "An unknown error occurred while trying to log you in.",
+                    Toast.LENGTH_SHORT
+                ).show()
+                }
+
+
+            }
+
+        }
+
+        else -> {}
+    }
 
     Scaffold(
         topBar = {
@@ -90,7 +166,18 @@ fun SignInScreen(navController: NavController, signInViewModel: SignInViewModel)
                         Text(
                             text = stringResource(R.string.forgot_password),
                             style = AppTheme.typography.subtitle,
-                            color = AppTheme.colors.onSurface
+                            color = AppTheme.colors.onSurface,
+                            modifier = Modifier.drawBehind {
+                                val strokeWidthPx = 2.dp.toPx()
+                                val verticalOffset = size.height - 2.sp.toPx()
+                                drawLine(
+                                    color = Color.DarkGray,
+                                    strokeWidth = strokeWidthPx,
+                                    start = Offset(0f, verticalOffset),
+                                    end = Offset(size.width, verticalOffset)
+                                )
+                            }
+
                         )
                     }
                 }
@@ -98,7 +185,10 @@ fun SignInScreen(navController: NavController, signInViewModel: SignInViewModel)
             }
             AppButton(
                 text = "Sign in",
-                onButtonClick = { },
+                onButtonClick = {
+                     signInViewModel.signInUser(username, password)
+                },
+                isLoading = isLoading,
                 enabled = username != "" && password != ""
             )
         }
@@ -125,5 +215,5 @@ fun SignInTopBar(
 @Preview
 @Composable
 fun SignInScreenPreview() {
-    SignInScreen(rememberNavController(), SignInViewModel())
+   // SignInScreen(rememberNavController(), SignInViewModel())
 }
