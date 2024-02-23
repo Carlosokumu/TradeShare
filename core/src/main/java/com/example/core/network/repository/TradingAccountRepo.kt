bddcbed8cdb8b54f69d1bde3f863fb68a90221ff
Carlos <carlosokumu254@gmail.com>
2024-02-23 08:30:59 +0300

    refactor

--- core/src/main/java/com/example/core/network/repository/TradingAccountRepo.kt
+++ core/src/main/java/com/example/core/network/repository/TradingAccountRepo.kt
@@ -2,6 +2,6 @@ package com.example.core.network.repository
 
 import com.example.core.network.data.models.EquityChart
 
-interface EquityChartRepo {
+interface TradingAccountRepo {
    suspend fun getQEquity(accountId: String,startTime: String,endTime: String): EquityChart
 }
