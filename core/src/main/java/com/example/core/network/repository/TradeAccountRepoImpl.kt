98135e30531581e9a35ad8247058981410421806
Carlos <carlosokumu254@gmail.com>
2024-02-26 16:10:26 +0300

    add account trades api calls

--- core/src/main/java/com/example/core/network/repository/TradeAccountRepoImpl.kt
+++ core/src/main/java/com/example/core/network/repository/TradeAccountRepoImpl.kt
@@ -4,6 +4,7 @@ import com.example.core.network.data.api.ApiCallResult
 import com.example.core.network.data.api.MtApi
 import com.example.core.network.data.api.TradeShareApi
 import com.example.core.network.data.api.safeApiCall
+import com.example.core.network.data.models.AccountTrades
 import com.example.core.network.data.models.EquityChart
 import com.example.core.network.data.models.Metrics
 import kotlinx.coroutines.CoroutineDispatcher
@@ -16,6 +17,10 @@ class TradeAccountRepoImpl(private val mtApi: MtApi,private val ioDispatcher: Co
         endTime: String
     ): ApiCallResult<EquityChart> = safeApiCall(ioDispatcher) {
         return@safeApiCall mtApi.getEquityChart(accountId, startTime, endTime)
+    }
+
+    override suspend fun getAccountTrades(accountId: String): ApiCallResult<AccountTrades> = safeApiCall(dispatcher = ioDispatcher) {
+        return@safeApiCall mtApi.getAccountTrades(accountId)
     }
 
 
