0dcf0ce503122092cf438c553835a497b2b9cebe
Carlos <carlosokumu254@gmail.com>
2024-04-22 09:23:32 +0300

    delete unused code

--- core/src/main/java/com/example/core/network/data/api/WebsocketConnectionListener.kt
+++
@@ -1,9 +0,0 @@
-package com.example.core.network.data.api
-
-import com.tinder.scarlet.Message
-
-interface WebsocketConnectionListener {
-    fun onConnected();
-    fun onMessageReceived(message: Message)
-    fun onDisconnected();
-}
