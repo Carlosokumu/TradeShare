cff3835fde728da82719aad9bd502dad368a2c74
Carlos <carlosokumu254@gmail.com>
2024-07-03 08:00:16 +0300

    refactor

--- app/src/main/java/com/android/swingwizards/common/AppBar.kt
+++ app/src/main/java/com/android/swingwizards/common/AppBar.kt
@@ -23,7 +23,6 @@ import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.android.swingwizards.R
-import com.android.swingwizards.models.Screen
 import com.android.swingwizards.theme.AppTheme
 
 @Composable
