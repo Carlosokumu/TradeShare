d1e6e12fdc84113eaa601d4d9118de87e7ca211f
Carlos <carlosokumu254@gmail.com>
2024-07-13 10:04:07 +0300

    include trader's stats in the historical views

--- app/src/main/java/com/android/swingwizards/models/TradeHistoryUiState.kt
+++ app/src/main/java/com/android/swingwizards/models/TradeHistoryUiState.kt
@@ -1,6 +1,7 @@
 package com.android.swingwizards.models
 
 import com.android.swingwizards.enums.TradeHistoryRange
+import com.carlos.model.DomainTraderStats
 import com.carlos.model.OpenTrade
 import com.carlos.model.Trade
 
@@ -10,7 +11,8 @@ sealed interface TradeHistoryUiState {
         val dataPoints: List<DataPoint>,
         val timeRangeSelected: TradeHistoryRange,
         val openTrades: List<OpenTrade>,
-        val historicalTrades: List<Trade>
+        val historicalTrades: List<Trade>,
+        val domainTraderStats: DomainTraderStats
     ) : TradeHistoryUiState
 
 
